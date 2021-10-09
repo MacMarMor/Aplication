d4b6cb9ebe8d0c939927d007190011eb2a05af4c
209399 <209399@CARBON.CTI.P.LODZ.PL>
2021-10-09 15:08:06 +0200

    Initial commit

---
+++ src/Aplication.java
@@ -0,0 +1,6 @@
+public class Aplication {
+    public static void main(String[] argv){
+        System.out.println("Hello World!\n Grupa: Chairs\n" +
+                "Rola: Deweloper \n GitHub Id Team Leadera: 60295795\n ");
+    }
+}
